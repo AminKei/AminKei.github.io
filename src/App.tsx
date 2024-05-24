711f19aff34541db7a14cc3e172796e18c2b8b0b
AminKei <moakeimasi@gmail.com>
2024-05-24 14:35:30 +0330

    add to git hub y

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Home } from "./Components/Home";
+import { CountryDetails } from "./Components/CountryDetails";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" data-testid="app-component-test-id">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/details/:name" element={<CountryDetails />} />
+          <Route path="*" element={<p>404 Page Not Found</p>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
